f498eea3005755373ef23c23ddeb0e3bbfec5842
Anthony David <anthony.david34@sfr.fr>
2024-02-10 16:30:55 +0100

    final

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -31,7 +31,14 @@ const CampSite = ({
         <div className="flexCenter gap-6">
           <span className="flex -space-x-4 overflow-hidden">
             {PEOPLE_URL.map((url) => (
-              <Image className=" inline-block h-10 w-10 rounded-full" key={url} src={url} alt="person" width={52} height={52} />
+              <Image
+                className=" inline-block h-10 w-10 rounded-full"
+                key={url}
+                src={url}
+                alt="person"
+                width={52}
+                height={52}
+              />
             ))}
           </span>
           <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
@@ -57,6 +64,19 @@ const Camp = () => {
           subtitle="Somewhere in the Wilderness"
           peopleJoined="50+ Joined"
         />
+      </div>
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+           <strong>Feeling Lost</strong> And Not Knowing The Way?</h2>
+          <p className="regular-14 xl:regular-16 text-white">
+            Starting from the anxiety of the climbers when visiting a new
+            climbing location, the possibility of getting lost is very large.
+            That's why we are here for those of you who want to start an
+            adventure
+          </p>
+          <Image src="/quote.svg" alt="camp-2" width={186} height={219} className="camp-quote"/>
+        </div>
       </div>
     </section>
   );
